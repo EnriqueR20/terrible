dabce4bbc1ad886e586091a8186ff11b47d8d48f
Joel Ramirez Garay <113269346+EnriqueR20@users.noreply.github.com>
2023-02-04 17:35:02 -0500

    Delete prisma/migrations directory

--- prisma/migrations/20230201234818_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Comentario" (
-    "id" SERIAL NOT NULL,
-    "comentario" TEXT NOT NULL,
-    "calificacion" INTEGER NOT NULL,
-
-    CONSTRAINT "Comentario_pkey" PRIMARY KEY ("id")
-);
